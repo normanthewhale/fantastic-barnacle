b67797b248f4d0befbac9d92cdf6863095b7cafc
William Hale <wmhale92@gmail.com>
2018-09-25 15:55:16 -0600

    made it prettier

--- problems/fizzbuzz.js
+++ problems/fizzbuzz.js
@@ -6,9 +6,9 @@ const fizzbuzz = (start, end) => {
       arr.push(i)
     }else if (i % 15 === 0) {
       arr.push("fizzbuzz");
-    }else if(i%5===0){
+    }else if(i % 5 === 0){
       arr.push("buzz");
-    }else if(i%3===0){
+    }else if(i % 3 === 0){
       arr.push("fizz");
     }else{
       arr.push(i);
